e0958d2234a28584133461feaa27084928e767b6
Lionbeat <brisenaudfaudeler199@gmail.com>
2024-12-31 11:11:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ function convertToBool(text, fault = 'true') {
 
 //--------------------------Mr Frank--------------------------------------
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "Enter Your SubZero Session ID",
+    SESSION_ID: process.env.SESSION_ID || "DMol3JYI#tIxxG0f8szBTCL5bn-KRglEMqaP-dphlRxrNYmNBTlk",
     CAPTION: process.env.CAPTION || "*©  Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ SᴜʙZᴇʀᴏ MD*",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
